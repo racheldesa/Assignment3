241071cc23b126748989092f44d24dc5966f8a5e
racheldesa <racheldesa@outlook.com>
2024-04-19 13:04:16 -0500

    View changer skeleton
    Added skeleton code for changing views in ModuleFiveViews.js

---
+++ frontend/src/ModuleFiveViews.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+export function FiveViews() {
+    const [addProduct, setAddProduct] = useState(false);
+    const [readProducts, setReadProducts] = useState(true);
+    const [updateProduct, setUpdateProduct] = useState(false);
+    const [deleteProduct, setDeleteProduct] = useState(false);
+    const [studentInfo, setStudentInfo] = useState(false);
+
+    function  addProductView() {
+
+    };
+
+    function readProductsView() {
+
+    };
+
+    function updateProductView() {
+
+    };
+
+    function deleteProductView() {
+
+    };
+
+    function studentInfoView() {
+
+    };
+
+    return (
+        <div>
+
+        </div>
+    );
+}
