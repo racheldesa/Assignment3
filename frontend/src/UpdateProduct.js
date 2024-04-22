bceefcd1a270134411907aa3373ea52b856f3008
racheldesa <racheldesa@outlook.com>
2024-04-22 13:52:50 -0500

    Added Confirmation view to UpdateProduct.js. TODO: show product card of queried item.

--- frontend/src/UpdateProduct.js
+++ frontend/src/UpdateProduct.js
@@ -13,17 +13,41 @@ function UpdateProduct() {
         setViewer(1);
     }
 
+    function SearchById() {
+        return (
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)} class="row g-3">
+                    <div class="col-auto">
+                        <input {...register("updateId", { required: true })} placeholder="Product ID"></input>
+                        {errors.updateId && <p>Product ID is required.</p>}
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3">Search</button>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+
+    function ConfirmProduct() {
+        return (
+            <div>
+                <h3>Confirm Product to Update: </h3>
+                <div id="search-result">
+
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-primary mb-3">Go Back</button>
+                    <button class="btn btn-primary mb-3">Confirm</button>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)} class="row g-3">
-                <div class="col-auto">
-                    <input {...register("updateId", { required: true })} placeholder="Product ID"></input>
-                    {errors.updateId && <p>Product ID is required.</p>}
-                </div>
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-primary mb-3">Search</button>
-                </div>
-            </form>
+            {viewer===0 && <SearchById />}
+            {viewer===1 && <ConfirmProduct />}
         </div>
     );
 }
