d09d7deb9febbee3055d97d3664db04fe379cc67
racheldesa <racheldesa@outlook.com>
2024-04-23 13:29:53 -0500

    Added functionality to UpdateProduct.SearchById to retrieve query ID value. TODO: display queried product for confimation view.

--- frontend/src/UpdateProduct.js
+++ frontend/src/UpdateProduct.js
@@ -1,53 +1,62 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
+import {Products} from "./Products";
+import {Categories} from "./Categories";
 
 function UpdateProduct() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    // const { register, handleSubmit, formState: { errors } } = useForm();
     const [dataF,setDataF] = useState({});
     const [viewer,setViewer] = useState(0);
+    const [query, setQuery] = useState('');
+    const [ProductsCategory, setProductsCategory] = useState(Products);
+    const [ProductID, setProductID] = useState(Products);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = data => {
-        console.log(data);
-        setDataF(data);
-        setViewer(1);
+    function onSubmit(data) {
+        console.log("Submit button clicked.");
+        setProductID(data.input_id);
+        console.log("Product ID: ", ProductID);
     }
 
     function SearchById() {
         return (
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)} class="row g-3">
-                    <div class="col-auto">
-                        <input {...register("updateId", { required: true })} placeholder="Product ID"></input>
-                        {errors.updateId && <p>Product ID is required.</p>}
-                    </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-primary mb-3">Search</button>
-                    </div>
-                </form>
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <p>
+                    <label htmlFor="input_id">
+                        Product ID: 
+                    </label>
+                    <input {...register("input_id")} id="input_id"/>
+                </p>
+                <input type="submit" value="Search"/>
+            </form>
         );
     };
 
     function ConfirmProduct() {
         return (
             <div>
-                <h3>Confirm Product to Update: </h3>
-                <div id="search-result">
 
-                </div>
-                <div class="col-auto">
-                    <button class="btn btn-primary mb-3">Go Back</button>
-                    <button class="btn btn-primary mb-3">Confirm</button>
-                </div>
             </div>
         );
     };
 
+    function UpdatePrice() {
+        return (
+            <div>
+                {/* <h3>Update Item Price:</h3>
+                <form onSubmit={handleSubmit(onSubmit)} */}
+            </div>
+        );
+    };
+
+
+
     return (
         <div>
             {viewer===0 && <SearchById />}
-            {viewer===1 && <ConfirmProduct />}
+            {/* {viewer===1 && <ConfirmProduct />}
+            {viewer===2 && <UpdatePrice />} */}
         </div>
     );
 }
