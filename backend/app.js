93a51992881d2ca1e45b549d9e4e2cd22a673335
racheldesa <racheldesa@outlook.com>
2024-04-24 09:19:14 -0500

    Implemented DELETE in backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -82,3 +82,26 @@ app.post("/addProduct", async (req,res) => {
         res.status(500).send({ error: 'An internal server error occurred' });
     }
 });
+
+app.delete("/deleteProduct/:id", async (req,res) => {
+    try {
+        const id = Number(req.params.id);
+
+        await client.connect();
+        console.log("Product to delete: ", id);
+
+        const query = { id: id };
+
+        // read data from product to delete to send it to frontend
+        const productDeleted = await db.collection("fakestore_catalog").findOne(query);
+        res.send(productDeleted);
+
+        // delete
+        const results = await db.collection("fakestore_catalog").deleteOne(query);
+        res.status(200);
+        res.send(results);
+    } catch (error) {
+        console.error("Error deleting product: ", error);
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+});
